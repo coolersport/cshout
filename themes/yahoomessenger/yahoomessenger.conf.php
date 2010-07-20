8e370edd23b71c8325547a24011600451a5349d3
tiendung.tran <tiendung.tran@f744622e-541d-11de-9a16-8934cb1294a2>
2010-07-20 02:04:39 +0000

    Added delete link for admin.

--- themes/yahoomessenger/yahoomessenger.conf.php
+++ themes/yahoomessenger/yahoomessenger.conf.php
@@ -5,4 +5,4 @@ else
 	$cshout_jsconfig['layout'] = '{shouts}<div>{panel_smileys}{panel_search}{panel_login}{panel_pages}{panel_help}</div><div>{help}{show_smileys}{show_search}{show_login}{show_pages}{show_navigator}<input type="checkbox" id="cs_autorefresh" title="Toggle chat mode" value="0"><label for="cs_autorefresh" id="cs_autorefresh_label">chat mode</label></div><div class="cs_formarea" style="margin:2px 0 2px">Send As {name} {status}</div><table id="cshout_entry"><tr><td><textarea id="cs_shout" rows="3" title="Message">Message</textarea></td><td width="1"><button type="button" id="cs_shoutit" title="Send message">Send</button></td></tr></table>';
 $cshout_jsconfig['layout_controls'] = '';
 $cshout_jsconfig['order'] = 'bottomup';
-$cshout_jsconfig['shout_template'] = '<div><b>{shouter_full}</b>: {shout}</div>';
+$cshout_jsconfig['shout_template'] = '<div><b>{shouter_full}</b> {cs_delete} <a href="#" onclick="alert(\'Shouter: {shouter_full}\\nWhen: {time} {date}\\nFrom IP: {ip}\');return(false)">(i)</a>: {shout}</div>';
